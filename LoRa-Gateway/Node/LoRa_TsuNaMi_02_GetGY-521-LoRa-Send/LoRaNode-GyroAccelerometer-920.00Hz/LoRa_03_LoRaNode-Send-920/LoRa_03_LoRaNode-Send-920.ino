f4d0b8ea1e746d20e071f604ba0d22e9ce29fc5b
powen ko <powenko@gmail.com>
2017-04-25 16:43:33 +0800

    add 920hz  demo

--- LoRa-Gateway/Node/LoRa_TsuNaMi_02_GetGY-521-LoRa-Send/LoRaNode-GyroAccelerometer-920.00Hz/LoRa_03_LoRaNode-Send-920/LoRa_03_LoRaNode-Send-920.ino
+++ LoRa-Gateway/Node/LoRa_TsuNaMi_02_GetGY-521-LoRa-Send/LoRaNode-GyroAccelerometer-920.00Hz/LoRa_03_LoRaNode-Send-920/LoRa_03_LoRaNode-Send-920.ino
@@ -1,5 +1,5 @@
 /*
- * iFrogLab IL-LORA1272 
+ * iFrogLab IL-LORA1272    920.00 => 92000 -> 0x16760 Gyro + Accelerometer
  * 功能:     ARDUINO  , IFROGLAB LORA, IL-LORA1272
  * 電源:     3.3V     ,Pin 3, VDD
  * 接地:     GND      ,Pin 1, GND
@@ -163,9 +163,10 @@ void LoRa_setup(){
 
   byte TXRX=2;  // Mode : Sleep(0x00)、StandBy(0x01)、Tx(0x02)、Rx(0x03)。  
   // Freq : 輸入915.00MHz先轉成91500再轉16進位方式填入0x1656C -> 0x01 0x65 0x6C，SX1272範圍(860.00 ~ 1020.00MHz)，SX1276範圍(137.00 ~ 1020.00MHz)。
-  byte Freq1=0x01;
-  byte Freq2=0x65;
-  byte Freq3=0x6c;
+  // Freq : 輸入920.00MHz先轉成92000再轉16進位方式填入0x16760 -> 0x01 0x67 0x60，SX1272範圍(860.00 ~ 1020.00MHz)，SX1276範圍(137.00 ~ 1020.00MHz)。
+  byte Freq1=0x01; //0x01;
+  byte Freq2=0x60; //0x65;
+  byte Freq3=0x60; // 0x6c;
   byte Power =0x3;  // Power:125k(0x01)、250k(0x2)、500k(0x3)。Default 500K。 
   
   byte* data=LoRa.Setup(TXRX, Freq1, Freq2, Freq3, Power);
